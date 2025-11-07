8eb250899baaa3a67bd31fb29b5efe80f323e20b
José Ulisses Fernandes Junior <11252401443@alunos.umc.br>
2025-11-07 16:08:38 -0300

    execução -prod

--- back_voluntariado/voluntariosdb.sql
+++ back_voluntariado/voluntariosdb.sql
@@ -1,4 +1,13 @@
 create database voluntariosdb;
 use voluntariosdb;
 
-create table voluntarios;
+CREATE TABLE voluntario (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(255) NOT NULL,
+    interesse VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM voluntario;
